6bcd912c2188f0badd3fc2e6528cb886281cbe9b
Yuxin Hou <yuxin.hou@Yuxins-MacBook-Pro.local>
2020-02-15 17:29:38 +0800

    Fibonacci: should_return_negative_1_when_calculate_given_position_is_greater_than_50

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -2,7 +2,7 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
     public static int calculate(int position) {
-        if (position < 1) {
+        if (position < 1 || position > 50) {
             return -1;
         }
 
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -10,4 +10,9 @@ public class FibonacciTest {
     public void should_return_negative_1_when_calculate_given_position_is_less_than_1() {
         assertThat(Fibonacci.calculate(0), is(-1));
     }
+
+    @Test
+    public void should_return_negative_1_when_calculate_given_position_is_greater_than_50() {
+        assertThat(Fibonacci.calculate(51), is(-1));
+    }
 }
